953eea9a91dae68d492be51052ca7e80d9f16d26
Angel Jostyn Arteaga Egusquiza <aarteage@LIM-C02H92RXQ05N.local>
2022-11-19 00:55:29 -0500

    r

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,8 @@ const app = express();
 
 // midlewares
 
-app.use(helmet())
-app.use(cors())
+// app.use(helmet())
+// app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use(morgan('dev'))
